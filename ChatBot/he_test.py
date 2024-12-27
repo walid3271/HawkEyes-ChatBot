a96317928542c511c7b0862794f13fae25356eaa
Munsi Walid Al Hassan Nizhu <walidmunsi@gmail.com>
2024-12-28 05:50:51 +0600

    'new'

--- ChatBot/he_test.py
+++ ChatBot/he_test.py
@@ -1,7 +1,4 @@
 import streamlit as st
-from langchain_community.document_loaders import WebBaseLoader
-import bs4
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 import os
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import google.generativeai as genai
@@ -14,49 +11,22 @@ from dotenv import load_dotenv
 load_dotenv()
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-def get_text():
-    urls = [
-        "https://www.hedigital.tech/about/"
-    ]
-
-    all_text_documents = []
-    for url in urls:
-        loader = WebBaseLoader(web_paths=(url,), bs_kwargs=dict(parse_only=bs4.SoupStrainer(
-                class_=("bg-[#181818] p-6 rounded-xl shadow-md",
-                        "container mx-auto my-2 xl:px-14 py-10",
-                        "container",
-                        "mx-auto my-10",
-                        "bg-primary py-20 mx-auto",
-                        "footer md:flex md:justify-center md:items-center text-[#ffffff]")
-            ))
-        )
-        text_documents = loader.load()
-        all_text_documents.extend(text_documents)
-    
-    return all_text_documents
-
-def get_text_chunks():
-    text_documents = get_text()
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
-    documents = text_splitter.split_documents(text_documents)
-    texts = [doc.page_content for doc in documents]
-    return texts
-
-def get_vector_store():
-    text_chunks = get_text_chunks()
-    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
-    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
-    vector_store.save_local("faiss_index")
-
 def get_conversational_chain():
+    # prompt_template = """
+    # Answer the question as detailed as possible from the provided context. If the answer is not in the context, say "answer is not available in the context" and do not provide an incorrect answer.\n\n
+    # Context:\n{context}\n
+    # Question:\n{question}\n
+    # Answer:
+    # """
+    
     prompt_template = """
-    Answer the question as detailed as possible from the provided context. If the answer is not in the context, say "answer is not available in the context" and do not provide an incorrect answer.\n\n
+    Answer the question as detailed as possible from the provided context.\n\n
     Context:\n{context}\n
     Question:\n{question}\n
     Answer:
     """
 
-    model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.5)
+    model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.9)
     prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
     chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
     return chain
@@ -72,7 +42,6 @@ def user_input(user_question):
     st.write("", response["output_text"])
 
 def main():
-    get_vector_store()
     st.set_page_config(page_title="HawkEyes")
     st.header("ChatBot For HawkEyes")
 
@@ -85,4 +54,4 @@ if __name__ == "__main__":
     main()
 
 
-# streamlit run he.py
+# ChatBot/streamlit run he_test.py localhost:8000
