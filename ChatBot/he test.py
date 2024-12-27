ab1fdcdd3b90f2708bf36ab267bc93d012b8f447
Munsi Walid Al Hassan Nizhu <walidmunsi@gmail.com>
2024-12-28 02:48:44 +0600

    'new'

---
+++ ChatBot/he test.py
@@ -0,0 +1,54 @@
+import streamlit as st
+from langchain_community.document_loaders import WebBaseLoader
+import bs4
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import os
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import google.generativeai as genai
+from langchain.vectorstores import FAISS
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+def get_conversational_chain():
+    prompt_template = """
+    Answer the question as detailed as possible from the provided context. If the answer is not in the context, say "answer is not available in the context" and do not provide an incorrect answer.\n\n
+    Context:\n{context}\n
+    Question:\n{question}\n
+    Answer:
+    """
+
+    model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.5)
+    prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
+    return chain
+
+def user_input(user_question):
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
+    docs = new_db.similarity_search(user_question)
+
+    chain = get_conversational_chain()
+    response = chain({"input_documents": docs, "question": user_question}, return_only_outputs=True)
+    
+    st.write("", response["output_text"])
+
+def main():
+    # get_vector_store()
+    st.set_page_config(page_title="HawkEyes")
+    st.header("ChatBot For HawkEyes")
+
+    user_question = st.text_input("Ask Questions About HawkEyes")
+
+    if user_question:
+        user_input(user_question)
+
+if __name__ == "__main__":
+    main()
+
+
+# streamlit run he.py
